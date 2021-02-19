0818f12b21dbc933f00985e5f82460b1f6b7efbd
K.T <64843479+Ghostglass@users.noreply.github.com>
2021-02-19 19:08:36 +0000

    has_one_attached_image for user profile

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,6 @@ class User < ApplicationRecord
   validates_presence_of :username, :password
   validates_uniqueness_of :username
   has_many :courses, dependent: :destroy
+  has_one_attached:image
   has_secure_password
 end
