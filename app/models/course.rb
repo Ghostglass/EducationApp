3306f8de012bb48bafa4310bd8972344b356e3f7
LiviaBas <livia.basiu@gmail.com>
2021-02-22 16:14:27 +0000

    front end

--- app/models/course.rb
+++ app/models/course.rb
@@ -4,4 +4,9 @@ class Course < ApplicationRecord
   has_many :subscriptions, dependent: :destroy
   validates_presence_of :user_id, :title
   has_one_attached :main_image
+
+  def get_course_user_name
+    user = User.find(self.user_id)
+    return user ? user.username : ""
+  end
 end
